f0b7df6b168e294b8791d056d0ace0a526c96a40
Jahziel Villasana <jahzielv@gmail.com>
2019-03-24 08:56:14 -0400

    added a mobile content component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,20 @@ import React from "react";
 import ContentComponent from "../components/ContentComponent";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Mobile from "../components/MobileContentComponent";
+import MediaQuery from "react-responsive";
 
 export default class HomePage extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <Header />
-                <ContentComponent />
+                <MediaQuery query="(max-device-width: 480px)">
+                    <Mobile />
+                </MediaQuery>
+                <MediaQuery query="(min-device-width: 1224px)">
+                    <ContentComponent />
+                </MediaQuery>
                 <Footer />
             </React.Fragment>
         );
